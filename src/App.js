cbc2c28580943f3922fdd47a0a21e516acf75702
SibongileT88 <sibongilet@beskbridge.com>
2025-12-04 11:02:24 +0200

    Changed the heading

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <p>Hello React</p>
+          <h1>My Dictionary App</h1>
         </header>
         <main>
           <Dictionary />
